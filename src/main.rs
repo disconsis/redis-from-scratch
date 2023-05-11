f6a04d91ace8a67be7c618fc2d77543dc8a5d31b
Ketan Kanishka <ketan.kanishka@aploris.com>
2023-05-11 22:58:28 +0530

    add skeleton for handling connections

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
 
 const DEFAULT_ADDR: &str = "127.0.0.1:6379";
+
+fn handle_conn(stream: TcpStream) {
+    match stream.peer_addr() {
+        Err(err) => {
+            eprintln!("[*] got no peer address for incoming connection!: {}", err);
+        }
+
+        Ok(peer) => {
+            println!("[*] handling connection from {}", peer);
+            std::thread::sleep(Duration::from_secs(100));
+            println!("[*] closing connection from {}", peer);
+        }
+    }
+}
 
 fn main() {
     let listener = TcpListener
@@ -8,10 +23,10 @@ fn main() {
         .expect("creating tcp listener");
     println!("[*] started listening on {}", DEFAULT_ADDR);
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+    for conn in listener.incoming() {
+        match conn {
+            Ok(stream) => {
+                std::thread::spawn(|| handle_conn(stream));
             }
             Err(e) => {
                 eprintln!("error: {}", e);
